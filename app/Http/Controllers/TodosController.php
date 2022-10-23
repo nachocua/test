2303c6a0414341ed48c4ef2808f4091aa86d0562
Hisu <nachocuatrin@gmail.com>
2022-10-23 17:36:10 -0300

    principal

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -24,4 +24,27 @@ class TodosController extends Controller
 
         return redirect()->route('todos')->with('success','Tarea creada correctamente');
     }
+
+    public function index(){
+        $todos = Todo::all();
+        return view('todos.index',['todos' => $todos]);
+    }
+    
+    public function show($id){
+        $todo = Todo::find($id);
+        return view('todos.show',['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id){
+        $todo = Todo::find($id);
+        $todo->title = $request->title;
+        $todo->save();
+        return redirect()->route('todos')->with('success', 'Tarea Actualizada');
+    }
+    
+    public function destroy($id){
+        $todo = Todo::find($id);
+        $todo->delete();
+        return redirect()->route('todos')->with('success', 'Tarea ha sido Eliminada');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodosController;
+use App\Http\Controllers\CategoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tareas', function () {
-    return view('todos.index');
-})->name('todos');
+
+Route::get('/tareas',[TodosController::class,'index'])->name('todos');
 
 Route::post('/tareas',[TodosController::class,'store'])->name('todos');
+
+Route::get('/tareas/{id}',[TodosController::class,'show'])->name('todos-edit');
+Route::patch('/tareas/{id}',[TodosController::class,'update'])->name('todos-update');
+Route::delete('/tareas/{id}',[TodosController::class,'destroy'])->name('todos-destroy');
+
+Route::resource('categories', CategoriesController::class);
